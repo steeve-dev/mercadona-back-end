8a6ba8f8992a7c3234b25c940dec4bd2f69cbd1a
steeve mellet <melletsteeve@orange.fr>
2023-03-01 16:55:49 +0100

    initial commit

---
+++ src/main/java/com/example/mercadonabackend/MercadonaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.mercadonabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadonaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercadonaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mercadonabackend/MercadonaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercadonabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadonaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
